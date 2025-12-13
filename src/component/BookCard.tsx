0f5ac7d70db00c2c9df19775fea057a811ba7f64
Paphangkon <paphangkon009@gmail.com>
2025-12-13 17:08:47 +0700

    add a tag link

--- src/component/BookCard.tsx
+++ src/component/BookCard.tsx
@@ -26,32 +26,35 @@ const BookCard = forwardRef<HTMLDivElement, BookCardProps>(({className, book, to
         description
     } = book;
     return (
-        <div ref={ref} className={cn(className, "flex rounded-md shadow gap-x-3 relative h-24 lg:h-32")} 
-            onClick={() => {
-                if(!editingMode) return;
-                toggleRemoveFav(id);
-            }}
-            {...props}
-        >
-            <div className="shrink-0 h-full w-20 lg:w-24 relative">
-                <Image 
-                src={imageUrl}
-                fill
-                alt={book.title}
-                className="rounded-md"
-            />
-            </div>
-            <div className="min-w-0 p-3 flex flex-col justify-between">
-                <div>
-                    <h3 className="truncate font-bold text-sm md:text-md">{title}</h3>
-                    <p className="truncate text-gray-600 text-xs md:text-sm">{author}</p>
+        <div ref={ref} className={cn(className, "flex justify-between rounded-md shadow h-24 lg:h-32")} {...props}>
+            <a href="/" className="min-w-0 flex-1">
+                <div className="flex h-full">
+                    <div className="shrink-0 h-full w-20 lg:w-24 relative">
+                        <Image 
+                        src={imageUrl}
+                        fill
+                        alt={book.title}
+                        className="rounded-md"
+                    />
+                    </div>
+                    <div className="min-w-0 p-3 flex flex-col justify-between">
+                        <div>
+                            <h3 className="truncate font-bold text-sm md:text-md">{title}</h3>
+                            <p className="truncate text-gray-600 text-xs md:text-sm">{author}</p>
+                        </div>
+                        <footer className="text-gray-500 text-xs md:text-sm">
+                            <p className="truncate">Chapter: {chapter}</p>
+                            <p className="truncate">Current: {currentChapter}</p>
+                        </footer>
+                    </div>
                 </div>
-                <footer className="text-gray-500 text-xs md:text-sm">
-                    <p className="truncate">Chapter: {chapter}</p>
-                    <p className="truncate">Current: {currentChapter}</p>
-                </footer>
-            </div>
-            <button className="absolute right-1 top-1" onClick={() => toggleFav(id)}>
+            </a>
+            <button className="border-l border-r-stone-300 p-2"
+                onClick={() => {
+                    if(!editingMode) toggleFav(id);
+                    else toggleRemoveFav(id);
+                }}
+            >
                 {(() => {
                     if(editingMode) return toRemove?<CircleCheck className="w-4 h-4 sm:w-6 sm:h-6"/>:<Circle className="w-4 h-4 sm:w-6 sm:h-6"/>;
                     return <Bookmark className={cn("w-4 h-4 sm:w-6 sm:h-6",book.isFav&&"fill-yellow-300")}/>;
--- src/component/ImageSlider.tsx
+++ src/component/ImageSlider.tsx
@@ -10,19 +10,21 @@ import { Banner } from "@/types/types";
 
 const ImageSlider = ({banners}:{banners: Banner[]}) => {
     return (
-        <Carousel>
-            <CarouselContent className="-ml-4">
-                {banners.map((banner) => {
-                    return (
-                        <CarouselItem key={banner.id} className="pl-4 basis-1/1 md:basis-1/2 lg:basis-1/3">
-                            <div className="relative w-full h-48 md:h-56 lg:h-72">
-                                <Image src={banner.imageUrl} fill alt={banner.title}/>
-                            </div>
-                        </CarouselItem>
-                    )
-                })}
-            </CarouselContent>
-        </Carousel>
+        <a href="/">
+            <Carousel>
+                <CarouselContent className="-ml-4">
+                    {banners.map((banner) => {
+                        return (
+                            <CarouselItem key={banner.id} className="pl-4 basis-1/1 md:basis-1/2 lg:basis-1/3">
+                                <div className="relative w-full h-48 md:h-56 lg:h-72">
+                                    <Image src={banner.imageUrl} fill alt={banner.title}/>
+                                </div>
+                            </CarouselItem>
+                        )
+                    })}
+                </CarouselContent>
+            </Carousel>
+        </a>
     )
 }
 
